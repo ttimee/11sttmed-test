1f6cd47d97911a697c578771b7db0782562868c8
Stefan Frede <stefan@frede.dev>
2023-02-11 23:56:15 +0100

    refactor(alpine): persist data (#480)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import './styles/main.css';
 
 import Alpine from 'alpinejs';
+import persist from '@alpinejs/persist';
 
 window.Alpine = Alpine;
+
+Alpine.plugin(persist);
+
+Alpine.data('banner', function () {
+  return {
+    show: this.$persist(false),
+    dismissed: this.$persist(false),
+
+    dismiss() {
+      this.show = false;
+      this.dismissed = true;
+    },
+
+    init() {
+      if (!this.dismissed) {
+        setTimeout(() => {
+          this.show = true;
+        }, 1500);
+      }
+    },
+  };
+});
 
 Alpine.start();
 
